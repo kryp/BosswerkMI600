2e5db1a41bac4a22330f09abf8e114a305a691d2
kryp <kryp@kryp.de>
2022-11-28 22:07:31 +0100

    added influxdb support

--- BosswerkMI600/__main__.py
+++ BosswerkMI600/__main__.py
@@ -2,51 +2,53 @@
 import requests
 from requests.exceptions import Timeout
 import os
-from paho.mqtt import client as mqtt
 import time, json, sys, math, datetime, re
 from datetime import datetime
 import subprocess
 import platform
 
+# from influxdb_client import InfluxDBClient, Point
+# client = InfluxDBClient(url=f"http://{influxdbhost}:8086", token=f'', org='-')
 
-################################
-# MQTT CONFIG
-################################
-mqtt_ip = 'localhost'
-mqtt_port = 1883
-topic = "/MI600_mqtt/data"
-client_id = "MI600_mqtt"
-mqtt_username = 'USERNAME'
-mqtt_password = 'PASSWORD'
+bosswerkIP = os.environ['BOSSWERKIP']
+htaccess_user = os.environ.get('BOSSWERK_USER', 'admin')
+htaccess_pw = os.environ.get('BOSSWERK_PW', 'admin')
+
+mqtt_topic = os.environ.get('BOSSWERK_MQTT_TOPIC', None)
+# mqtt_topic = "/MI600_mqtt/data"
+# mqtt_client_id = "MI600_mqtt"
+# mqtt_username = 'USERNAME'
+# mqtt_password = 'PASSWORD'
+
+influxdbhost = os.environ.get('BOSSWERK_INFLUXDB_HOST', 'localhost')
+influxdbport = os.environ.get('BOSSWERK_INFLUXDB_PORT', '8086')
+influxdbucket = os.environ.get('BOSSWERK_INFLUXDB_BUCKET', 'vzlogger')
+influxdburl = f"http://{influxdbhost}:{influxdbport}"
+
+webinterface_url = f'http://{bosswerkIP}/status.html'
+ping_try_count = 3
 
 
-################################
-# MI600 Login
-################################
-htaccess_user = 'admin'
-htaccess_pw = '12345678'
-bosswerkIP = '192.168.x.x'
-ping_try_count = 10     #multiplied with 3 seconds sleep between each loop => 10*3 = 30 seconds try to connect the inverter then kill the script
-webinterface_url = 'http://192.168.x.x/status.html'
-
-
-
+try:
+    from paho.mqtt import client as mqtt
+except ImportError:
+    print("could not find paho.mqtt: pip install paho.mqtt")
 
 def connectMQTT(ip, port):
- #https://github.com/fr00sch/bosswerk_mi600_solar
- #https://pypi.org/project/paho-mqtt/
- client = mqtt.Client()
- client.username_pw_set(mqtt_username, mqtt_password)
- client.on_connect = on_connect
- client.on_message = on_message
- #with mqtt.Client(client_id="0", clean_session=True, userdata=None, protocol="MQTTv311", transport="tcp") as client:
- client.connect(ip , port, 60)
- return client
+    #https://github.com/fr00sch/bosswerk_mi600_solar
+    #https://pypi.org/project/paho-mqtt/
+    client = mqtt.Client()
+    client.username_pw_set(mqtt_username, mqtt_password)
+    client.on_connect = on_connect
+    client.on_message = on_message
+    #with mqtt.Client(client_id="0", clean_session=True, userdata=None, protocol="MQTTv311", transport="tcp") as client:
+    client.connect(ip , port, 60)
+    return client
 
 def sendData(client, webdata_now_p, webdata_today_e, webdata_total_e):
     #https://github.com/fr00sch/bosswerk_mi600_solar
     startmsg = json.dumps({"device": {"status": {"clientname":'MI600', "status":'Online', "power":webdata_now_p, "today":webdata_today_e, "total":webdata_total_e, "lastDateUpdate":datetime.today().strftime('%Y-%m-%d %H:%M:%S')}}}, skipkeys = True, allow_nan = False);
-    client.publish(topic, startmsg, qos=0, retain=False)
+    client.publish(mqtt_topic, startmsg, qos=0, retain=False)
     client.disconnect()
 
 # The callback for when the client receives a CONNACK response from the server.
@@ -60,7 +62,7 @@ def on_connect(client, userdata, flags, rc):
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     #https://github.com/fr00sch/bosswerk_mi600_solar
-    print(msg.topic+":"+str(msg.payload))  
+    print(msg.topic+":"+str(msg.payload))
 
 
 
@@ -86,7 +88,7 @@ def find_target_value(target, hp_source):
     #print("start: {}" .format(find_value_start))
     find_value_end = hp_source.find("\"", find_value_start+1)
     #print("end: {}" .format(find_value_end))
-          
+
   get_target_back = hp_source[find_value_start+1:find_value_end]
   return(get_target_back)
 
@@ -98,7 +100,7 @@ def get_Solar_values():
     except Timeout:
         print('I waited far too long')
     else:
-        print('The request got executed')
+        # print('The request got executed')
         if r.status_code == 200:
             hp_source = str(r.text)
             #print(hp_source)
@@ -123,17 +125,25 @@ def get_Solar_values():
                 if not (re.search('---',ret2) == True):
                     total = ret2
                     #print(ret2)
-                client = connectMQTT(mqtt_ip, mqtt_port)
-                sendData(client, power, today, total)
-            
+                # client = connectMQTT(mqtt_ip, mqtt_port)
+                data = ( power, today, total)
+                print(data)
+                if power != "" and influxdbhost:
+                    influxpost = f"{influxdburl}/write?db=vzlogger&u=fhem&p=pw"
+                    # 1  6-7-0,uuid=4c920356-abfe-11e9-a89b-c74ec9b5ea02,foo=bar value=124.472455 1656242453142
+                    r = requests.post(influxpost, data=f"solarpower value={power}")
+                    print(f"influxresult: {r.status_code}")
+                    # p = Point("solarpower").field("value", int(power))
+                    # write_api.write(bucket=bucket, record=p)
+                if power != "" and mqtt_topic:
+                    client = connectMQTT(mqtt_ip, mqtt_port)
+                    sendData(client, power, today, total)
         else:
             print(r.status_code)
 
         #close connection
         r.close()
-        print("Connection Closed")
-
-
+        # print("Connection Closed")
 
 
 if __name__=='__main__':
@@ -147,8 +157,20 @@ if __name__=='__main__':
           getDataCountPing = getDataCountPing + 1
           time.sleep(3)
           if getDataCountPing == ping_try_count:
-            startmsg = json.dumps({"device": {"status": {"clientname":'MI600', "status":"Offline", "power":0.0, "lastDateUpdate":datetime.today().strftime('%Y-%m-%d %H:%M:%S')}}}, skipkeys = True, allow_nan = False);
-            client = connectMQTT(mqtt_ip, mqtt_port)
-            client.publish(topic, startmsg, qos=0, retain=False)
-            client.disconnect()
-          
+            startmsg = json.dumps(
+                {"device": {
+                    "status": {
+                        "clientname":'MI600',
+                        "status":"Offline",
+                        "power": 0.0,
+                        "lastDateUpdate":datetime.today().strftime('%Y-%m-%d %H:%M:%S')
+                            }
+                        }
+                    },
+                skipkeys = True, allow_nan = False)
+            print(startmsg)
+            if mqtt_topic:
+                client = connectMQTT(mqtt_ip, mqtt_port)
+                client.publish(mqtt_topic, startmsg, qos=0, retain=False)
+                client.disconnect()
+
--- README.md
+++ README.md
@@ -6,11 +6,24 @@ This little python script will readout quick and dirty the current values of the
 2. use requests to get the HTML code of the web interface of the converter
 3. find position of "var webdata_now_p =" / "var webdata_today_e =" / "var webdata_total_e ="
 4. find following the next two " and read the values in between
-5. check if the values are logical and if yes use a mqtt script to send the values to your mqtt broker
-6. after that you can use the mqtt message in openhab or other automation systems 
 
+5. check if the values are logical and if yes use
+6. a mqtt script to send the values to your mqtt broker (if defined)
+7. send data via influxdb (if defined)
 
 Note:
 Sometimes the converter has a timeout of some seconds or minutes. I do not know what the converter is doing in this time. Maybe there is an upload to Solarman???
 
 I started to use the script from https://github.com/fr00sch/bosswerk_mi600_solar. But because it use selenium there is a high cpu workload if the script is working. So I switched to my solution with less impact of workload. I have reused some script parts from https://github.com/fr00sch/bosswerk_mi600_solar
+
+
+## config
+Use environment variables:
+
+
+## Systemd:
+cp templates/bosswerkmi600* /etc/systemd/system
+systemctl daemon-reload
+systemctl enable bosswerkmi600.service
+systemctl enable bosswerkmi600.timer
+systemctl start bosswerkmi600.timer
